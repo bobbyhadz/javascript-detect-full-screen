3be4abe70fe9d62e741ed1313c4a03f7b0df1430
bobbyhadz <opmftw@gmail.com>
2024-03-07 10:33:26 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log('bobbyhadz.com');
+
+if (window.innerHeight === screen.height) {
+  console.log('The browser is in fullscreen mode');
+} else {
+  console.log('The browser is NOT in fullscreen mode');
+}
